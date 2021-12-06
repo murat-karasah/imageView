ab69898d8a9edb542c8117d0b23ad4ecc1e0fd45
Murat Karasah <mkarasah@gmail.com>
2021-12-06 14:18:13 +0300

    Add project files.

---
+++ imageView/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace imageView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_ilk = new System.Windows.Forms.Button();
+            this.btn_geri = new System.Windows.Forms.Button();
+            this.btn_random = new System.Windows.Forms.Button();
+            this.btn_slayt = new System.Windows.Forms.Button();
+            this.ileri = new System.Windows.Forms.Button();
+            this.btn_son = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(236, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(552, 342);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 426);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btn_ilk
+            // 
+            this.btn_ilk.Location = new System.Drawing.Point(268, 377);
+            this.btn_ilk.Name = "btn_ilk";
+            this.btn_ilk.Size = new System.Drawing.Size(76, 23);
+            this.btn_ilk.TabIndex = 2;
+            this.btn_ilk.Text = "<<";
+            this.btn_ilk.UseVisualStyleBackColor = true;
+            this.btn_ilk.Click += new System.EventHandler(this.btn_ilk_Click);
+            // 
+            // btn_geri
+            // 
+            this.btn_geri.Location = new System.Drawing.Point(350, 377);
+            this.btn_geri.Name = "btn_geri";
+            this.btn_geri.Size = new System.Drawing.Size(76, 23);
+            this.btn_geri.TabIndex = 2;
+            this.btn_geri.Text = "<";
+            this.btn_geri.UseVisualStyleBackColor = true;
+            this.btn_geri.Click += new System.EventHandler(this.btn_geri_Click);
+            // 
+            // btn_random
+            // 
+            this.btn_random.Location = new System.Drawing.Point(432, 377);
+            this.btn_random.Name = "btn_random";
+            this.btn_random.Size = new System.Drawing.Size(76, 23);
+            this.btn_random.TabIndex = 2;
+            this.btn_random.Text = "Random";
+            this.btn_random.UseVisualStyleBackColor = true;
+            this.btn_random.Click += new System.EventHandler(this.btn_random_Click);
+            // 
+            // btn_slayt
+            // 
+            this.btn_slayt.Location = new System.Drawing.Point(514, 377);
+            this.btn_slayt.Name = "btn_slayt";
+            this.btn_slayt.Size = new System.Drawing.Size(76, 23);
+            this.btn_slayt.TabIndex = 2;
+            this.btn_slayt.Text = "Slayt";
+            this.btn_slayt.UseVisualStyleBackColor = true;
+            this.btn_slayt.Click += new System.EventHandler(this.btn_slayt_Click);
+            // 
+            // ileri
+            // 
+            this.ileri.Location = new System.Drawing.Point(596, 377);
+            this.ileri.Name = "ileri";
+            this.ileri.Size = new System.Drawing.Size(76, 23);
+            this.ileri.TabIndex = 2;
+            this.ileri.Text = ">";
+            this.ileri.UseVisualStyleBackColor = true;
+            this.ileri.Click += new System.EventHandler(this.ileri_Click);
+            // 
+            // btn_son
+            // 
+            this.btn_son.Location = new System.Drawing.Point(678, 377);
+            this.btn_son.Name = "btn_son";
+            this.btn_son.Size = new System.Drawing.Size(76, 23);
+            this.btn_son.TabIndex = 2;
+            this.btn_son.Text = ">>";
+            this.btn_son.UseVisualStyleBackColor = true;
+            this.btn_son.Click += new System.EventHandler(this.btn_son_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_son);
+            this.Controls.Add(this.ileri);
+            this.Controls.Add(this.btn_slayt);
+            this.Controls.Add(this.btn_random);
+            this.Controls.Add(this.btn_geri);
+            this.Controls.Add(this.btn_ilk);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn_ilk;
+        private System.Windows.Forms.Button btn_geri;
+        private System.Windows.Forms.Button btn_random;
+        private System.Windows.Forms.Button btn_slayt;
+        private System.Windows.Forms.Button ileri;
+        private System.Windows.Forms.Button btn_son;
+    }
+}
+
---
+++ imageView/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] resimler;
+        int i;
+        int hangiresim = 0;
+        int gec = 0;
+        int button1 = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resimler = Directory.GetFiles("../../images","*.jpg");
+            foreach (string item in resimler)
+            {
+                Button btn = new Button();
+                btn.Width = btn.Height = 68;
+                btn.Padding = new Padding(0);
+                btn.BackgroundImage = Image.FromFile(item);
+                btn.BackgroundImageLayout = ImageLayout.Stretch;
+                btn.Tag = i++;
+                flowLayoutPanel1.Controls.Add(btn);
+                btn.Click += btn_click;
+                   
+            }
+
+            
+
+            
+        }
+        void btn_click(object sender, EventArgs e)
+        {
+            Button basilan = sender as Button;
+            pictureBox1.Image = Image.FromFile(resimler[Convert.ToInt32(basilan.Tag)]);
+        }
+
+        private void btn_ilk_Click(object sender, EventArgs e)
+        {
+            hangiresim = 0;
+            pictureBox1.Image = Image.FromFile(resimler[hangiresim]);
+
+            
+        }
+
+        private void ileri_Click(object sender, EventArgs e)
+        {
+            hangiresim++;
+            if (hangiresim>resimler.Length-1)
+            {
+                hangiresim = 0;
+            }
+            pictureBox1.Image = Image.FromFile(resimler[hangiresim]);
+
+        }
+
+        private void btn_son_Click(object sender, EventArgs e)
+        {
+            hangiresim = resimler.Length - 1;
+            pictureBox1.Image = Image.FromFile(resimler[hangiresim]);
+
+        }
+
+        private void btn_random_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            pictureBox1.Image = Image.FromFile(resimler[rnd.Next(0, resimler.Length - 1)]);
+
+        }
+
+        private void btn_slayt_Click(object sender, EventArgs e)
+        {
+            
+            button1++;
+            timer1.Start();
+            if (button1==2)
+            {
+                timer1.Stop();
+                button1 = 0;
+            }
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gec++;
+            if (hangiresim > resimler.Length - 1)
+            {
+                hangiresim = 0;
+            }
+            pictureBox1.Image = Image.FromFile(resimler[hangiresim++]);
+
+        }
+
+        private void btn_geri_Click(object sender, EventArgs e)
+        {
+            if (hangiresim ==0)
+            {
+                hangiresim = 0;
+            }
+            else
+            {
+                hangiresim--;
+            }
+            pictureBox1.Image = Image.FromFile(resimler[hangiresim]);
+
+        }
+    }
+}
